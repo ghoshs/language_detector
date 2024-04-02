9649d7eb99d1979aca87d810a60301461fd5f2f9
ghoshs <5251447+ghoshs@users.noreply.github.com>
2024-04-02 23:36:59 +0200

    min frequency to remove noise

--- build_profile.py
+++ build_profile.py
@@ -6,17 +6,17 @@ from prepare_data import prepare_data
 MAX_NGRAM = 3
 PATH = './profiles'
 
-def one_gram_frequency(lines):
+def one_gram_frequency(lines, min_freq):
 	one_gram_dict = defaultdict(int)
 	for line in lines:
 		for char in line:
 			# code_point = ord(char)
 			one_gram_dict[char] += 1
-			
+	one_gram_dict = {char: freq for char, freq in one_gram_dict.items() if freq >= min_freq}
 	return one_gram_dict
 
 
-def ngram_frequency(lines, n=2):
+def ngram_frequency(lines, n, min_freq):
 	ngram_dict = defaultdict(int)
 	for line in lines:
 		if len(line) < n:
@@ -25,19 +25,18 @@ def ngram_frequency(lines, n=2):
 			ngram = line[idx:idx+n]
 			# code_point_tuple = ";".join([ord(char) for char in ngram])
 			ngram_dict[ngram] += 1
-
+	ngram_dict = {char: freq for char, freq in ngram_dict.items() if freq >= min_freq}
 	return ngram_dict
 
 
-def build_profile(lang, lines, max_n=1):
+def build_profile(lines, max_n=1, min_freq=10):
 	lang_profile = {}
 	assert max_n <= MAX_NGRAM
 
-	lang_profile['1gram'] = one_gram_frequency(lines)
+	lang_profile['1gram'] = one_gram_frequency(lines, min_freq)
 	if max_n > 1:
 		for n in range(2,max_n+1):
-			lang_profile[str(n)+'gram'] = ngram_frequency(lines, n=2)
-	print(lang_profile)
+			lang_profile[str(n)+'gram'] = ngram_frequency(lines, n, min_freq)
 	return lang_profile
 
 
@@ -55,5 +54,5 @@ if __name__ == '__main__':
 			'wili-2018/labels.csv', 
 			['ben', 'deu', 'eng'])
 	for lang, lines in langugae_lines.items():
-		lang_profile = build_profile(lang, lines[0:5], 3)
+		lang_profile = build_profile(lines, 3, 10)
 		save_profile(lang_profile, lang)
